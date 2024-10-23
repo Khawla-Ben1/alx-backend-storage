9d5490e0359440699ad2568a5809a3b72a30fd8a
Khawla Ben hammou <kben-ham@e2r1p2.1337.ma>
2024-10-23 21:01:38 +0100

    push

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+Implementing an expiring web cache and tracker
+"""
+
+import redis
+import requests
+from typing import Callable
+from functools import wraps
+
+redis = redis.Redis()
+
+
+def wrap_requests(fn: Callable) -> Callable:
+    """Decorator that wraps d given func & provides caching functionality
+    Returns:
+        The wrapped function
+    """
+
+    @wraps(fn)
+    def wrapper(url):
+        """Wrapper function that adds caching logic to the decorated func
+        Returns:
+            The HTML content of the page
+        """
+        redis.incr(f"count:{url}")
+        cached_response = redis.get(f"cached:{url}")
+        if cached_response:
+            return cached_response.decode('utf-8')
+        result = fn(url)
+        redis.setex(f"cached:{url}", 10, result)
+        return result
+    return wrapper
+
+
+@wrap_requests
+def get_page(url: str) -> str:
+    """Fetches the HTML content of a web page.
+    Returns:
+        The HTML content of the web page
+    """
+    response = requests.get(url)
+    return response.text
